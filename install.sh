a8093d3058069b4754f5acb2e73ea73d7b5bddad
Chris Salzberg <chris@dejimata.com>
2021-05-18 14:44:16 +0900

    Add .pryrc

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ fi
 ln -sf ~/dotfiles/vimrc ~/.vimrc
 ln -sf ~/dotfiles/zshrc ~/.zshrc
 ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/pryrc ~/.pryrc
 
 mkdir -p ~/.vim/pack/vendor/start
 mkdir -p ~/.vim/colors
